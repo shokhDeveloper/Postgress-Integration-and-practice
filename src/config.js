a34188513584fc0e029386284011ce6841b28540
Shohijahon Musinkulov <110657735+shokhDeveloper@users.noreply.github.com>
2024-05-13 14:27:56 +0500

    Edit postgres Integration

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 export const PORT = process.env.PORT || 4000;
 export const posgtresIntegration = {
     host: "localhost",
-    password: "82850406m",
+    password: "82850406sh",
     user: "postgres",
     database: "books"
 }
